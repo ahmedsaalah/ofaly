3aa5e67fbd968df83a6102e855f5157568bf2b8d
salah <a7mad.sala7@live.com>
2018-04-13 17:27:54 +0200

    Eee

--- connection.py
+++ connection.py
@@ -13,42 +13,49 @@ from flask import session as login_session
 import httplib2
 import requests
 import json
+import tweepy
+from tweepy import OAuthHandler
+
 
 
 
 
 app =Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/Ofaly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:root@localhost/Ofaaly'
 
 db = SQLAlchemy(app)
 
 class users(db.Model):
    id = db.Column('id', db.Integer, primary_key = True)
-   fb_id = db.Column(db.Integer)
+   tw_id = db.Column(db.Integer)
    name = db.Column(db.String(100))  
    picture = db.Column(db.String(512))
 
 
-   def __init__(self, name=None, fb_id=None, picture=None):
+   def __init__(self, name=None, tw_id=None, picture=None):
         self.name = name
-        self.fb_id = fb_id
+        self.tw_id = tw_id
         self.picture = picture
 
 
 
 class posts(db.Model):
    id = db.Column('id', db.Integer, primary_key = True)
-   fb_id = db.Column(db.Integer)
+   tw_id = db.Column(db.Integer)
+   acc_id = db.Column(db.Integer)
    message = db.Column(db.String(512))
    createdTime = db.Column(db.String(70))
 
 
-   def __init__(self, fb_id=None ,message=None ,createdTime=None) :
-
-        self.fb_id = fb_id
-        self.message = message
+   def __init__(self, acc_id=None, tw_id=None ,message=None ,createdTime=None) :
+        self.acc_id = acc_id
+        self.tw_id = tw_id.encode('ascii', 'ignore').decode('ascii')
+        self.message = message.encode('ascii', 'ignore').decode('ascii')
         self.createdTime = createdTime
+   def __repr__(self):
+        
+        return 'tweet %s %s %s' % (self.message, self.createdTime , self.acc_id)
 
 
 
-
+db.create_all()
--- main.py
+++ main.py
@@ -1,86 +1,201 @@
 from connection import *
 
 
+CONSUMER_KEY = 'qf8PgvKdBPu2RynHWfpA9MKOL'
+CONSUMER_SECRET = 'ROjmfNDVOad47cvSXRS3rPtrn0r0zceAieajR46E8EmPLZ8lVL'
+ACCESS_KEY = '319102708-Sjt9HwUsZPbcwSAaOwHUeUg37mTAJyX6tv09nSOJ'
+ACCESS_SECRET = 'uLUpsT1hXEIIc8m34vmAFG81Ne08bwpJwQFsq49so2RJA'
 
+
+
+
+def UserDataTwitter(Id):
+    auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    api = tweepy.API(auth)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+    #search
+    api = tweepy.API(auth)
+    try :   
+        user = api.lookup_users(user_ids=[Id])
+
+    except:
+        return json.dumps({"error": "No user matches for this id"  })
+
+
+    dataDetails = user[0]
+    user = users(name = dataDetails.name , tw_id = Id , picture = dataDetails.profile_image_url_https)
+
+    return user        
+
+
+
+
+@app.route("/tweets/<Id>", methods=['POST','GET'])
+def UsertweetsTwitter(Id):
+    auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+    api = tweepy.API(auth)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+
+    #search
+    api = tweepy.API(auth)
+   
+    try :   
+        posts = api.user_timeline(id=Id, count = 25 ,include_rts = True)
+
+    except:
+        return json.dumps({"error": "No user matches for this id"  })
+    return posts   
+    # return str(posts[0].text) +"   "+ str(posts[0].created_at) +"   "+ str(posts[0].id_str) +"   "+ str(posts[1].id_str)  
 
 
 '''   it  scrap user data and save to DB '''
-@app.route("/users/<facebookId>", methods=['POST','GET'])
-def UserData(facebookId):
+@app.route("/users/<twId>", methods=['POST','GET'])
+def UserData(twId):
 
     local = request.args.get('local')
     if local == None:
         local = False
     else :
         local = True
-        userData = getByFbID(facebookId) 
+        userData = getByTwID(twId) 
 
         if userData is None :
-            return  str(json.dumps({"error": "User not exist in the Database"  }))
+            return  json.dumps({"error": "User not exist in the Database"  })
         else :
-            return  str(json.dumps({"name": userData.name , "picture" :userData.picture , "fb_id" :userData.fb_id }, indent = 4 ,separators=(',', ': ')))            
+            return  json.dumps({"name": userData.name , "picture" :userData.picture , "tw_id" :userData.tw_id }, sort_keys=True,
+                   indent=4, separators=(',', ': '))            
 
 
-    fileData = json.loads(open('fb_client_secrets.json', 'r').read())[
-        'web']
-        
-    app_id = fileData['app_id']
-    app_secret = fileData['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=client_credentials&&client_id=%s&client_secret=%s' % (app_id, app_secret)
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    tokenData = result.split("&")[0]
-    token=json.loads(tokenData)
-
-    url = 'https://graph.facebook.com/v2.8/%s?access_token=%s&fields=name,id,email' % (facebookId,token["access_token"])
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    dataDetails = json.loads(result)
+   
+    user = UserDataTwitter(twId)
     
-    if 'error' in dataDetails :
-        return  str(json.dumps({"error": "ID %s does not exist" % facebookId }))
 
-    # Get user picture
-    url = 'https://graph.facebook.com/v2.4/%s/picture?access_token=%s&redirect=0&height=720&width=720' %  (facebookId,token["access_token"])
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    data = json.loads(result)
-    picture = data["data"]["url"]
 
-    userObj = CheckIfexist(dataDetails,picture)
-    return str(json.dumps({"name": userObj.name , "picture" :userObj.picture , "fb_id" :userObj.fb_id  }, indent=4 ,separators=(',', ': '))) 
+ 
 
+    userObj = CheckIfexist(user)
+    return json.dumps({"name": userObj.name , "picture" :userObj.picture , "tw_id" :userObj.tw_id  }, indent=4 ,separators=(',', ': '))
+
+
+@app.route("/users/<accID>/posts", methods=['POST','GET'])
+def getposts(accID):
+
+    local = request.args.get('local')
+    if local == None:
+        local = False
+    else :
+        local = True
+        tweets = getTweetsByID(accID) 
+
+        if tweets is None :
+            return  json.dumps({"error": "User not exist in the Database"  })
+        else :
+            return  printTweets(tweets,accID)          
+
+
+   
+    tweetsnew = UsertweetsTwitter(accID)
+    
+
+
+ 
+
+    tweets = CheckIfTweetsExist(tweetsnew,accID)
+    return printTweets(tweets,accID)
+
+def printTweets(tweets,accID):
+    json_Data =[]
+    i=0
+    # print(str(tweets))
+    for tweet in tweets:
+
+    
+        post = posts( acc_id= accID, tw_id=tweet.id_str ,message=tweet.text ,createdTime=tweet.created_at)
+
+        json_Data.append(str(post))
+
+    return json.dumps(json_Data)
+
+def CheckIfTweetsExist(tweets , accid):
+    tweetsdb = getTweetsByID(accid)
+   
+    if len(tweetsdb) ==0 :
+        
+        tweets = Addtweets(tweets,accid)
+
+    else :
+        tweets = UpdateTweets(tweetsdb,tweets)
+
+
+
+    return tweets
+
+def getTweetsByID(accID):
+    tweets = posts.query.filter_by(acc_id=accID).all()
+    return tweets
+
+
+def Addtweets(tweets,accID):
+    i=0
+    for tweet in tweets:
+
+        post = posts( acc_id= accID, tw_id=tweet.id_str ,message=tweet.text ,createdTime=tweet.created_at)
+        db.session.add(post)
+        db.session.commit()
+        print(i)
+        i=i+1
+
+    return tweets
+def UpdateTweets(tweetsdb,tweets):
+    i=0
+    if tweetsdb[0].tw_id == tweets[0].id_str:
+        return tweets
+    else :
+        for tweet in tweetsdb:
+            tweet.tw_id =  tweets[i].id_str
+            tweet.message = tweets[i].text
+            tweet.createdTime =tweets[i].created_at
+            db.session.commit()
+            i = i + 1
+
+
+        
+    return tweets
+
+
+    
 
 
 ''' end'''
-def getByFbID(id):
-    userObj = users.query.filter_by(fb_id=id).first()
+def getByTwID(id):
+    userObj = users.query.filter_by(tw_id=id).first()
     return userObj
 
-def AddUser(dataDetails,picture):
-    userObj = users(fb_id=dataDetails["id"] ,name=dataDetails["name"],  picture =picture)
+def AddUser(user):
+    userObj = users(tw_id=user.tw_id ,name=user.name,  picture =user.picture)
     db.session.add(userObj)
     db.session.commit()
     return userObj
     
-def CheckIfexist(dataDetails,picture):
-    userObj = users.query.filter_by(fb_id=dataDetails["id"]).first()
+def CheckIfexist(user):
+    userObj = users.query.filter_by(tw_id=user.tw_id).first()
     if userObj is None :
-        userObj = AddUser(dataDetails,picture)
+        userObj = AddUser(user)
 
     else :
-        userObj = UpdateUser (userObj,dataDetails,picture)
+        userObj = UpdateUser (userObj,user)
 
 
 
     return userObj
 
-def UpdateUser (userObj,dataDetails,picture):
+def UpdateUser (userObj,user):
     
-    if userObj.name != dataDetails["name"]:
-        userObj.name = dataDetails["name"]
-    if userObj.picture != picture:
-        userObj.picture = picture
+    if userObj.name != user.name :
+        userObj.name = user.name 
+    if userObj.picture != user.picture:
+        userObj.picture = user.picture
     db.session.commit()
     return userObj
 
